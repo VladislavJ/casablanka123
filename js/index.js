3149acbf5e5cc6a442636998ec2fd12394a5f1de
VladislavJ <dkfl123@bigmir.net>
2016-02-13 13:31:56 +0200

    123
    123

---
+++ js/index.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	$(".owl-carousel").owlCarousel({
+	autoplay:false,
+	animateOut: 'fadeOut',
+	items:2,
+	itemsScaleUp: true,
+	margin:0,
+	stagePadding:0,
+	smartSpeed:450,
+	dots:true,
+	slideSpeed: 1,
+	autoplay:true,
+	mouseDrag:false
+	});
+});
+
+function openMe(){
+	x=document.getElementById("video_open");
+	y=document.getElementById("icon_close");
+	x.style.visibility="visible";
+	y.style.visibility="hidden";
+}
