9b65dcc0dabdeffa379cd434986993b4669b5ec4
VladislavJ <dkfl123@bigmir.net>
2016-02-15 19:05:51 +0200

    123
    123

--- js/index.js
+++ js/index.js
@@ -1,14 +1,13 @@
 $(document).ready(function(){
 	$(".owl-carousel").owlCarousel({
 	autoplay:false,
-	animateOut: 'fadeOut',
 	items:1,
 	itemsScaleUp: true,
 	margin:0,
 	stagePadding:0,
 	smartSpeed:450,
 	dots:true,
-	slideSpeed: 1,
+	slideSpeed: 800,
 	// autoplay:true,
 	mouseDrag:false
 	});
